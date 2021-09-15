72d1c63090dcfef95c067443ba234d874dbe4cbe
Gaurav Maharjan <rijanmaharjan999@gmail.com>
2021-09-15 09:01:31 +0545

    File about declaring variable and constant including array and object

---
+++ index.js
@@ -0,0 +1,31 @@
+let a = 1;
+let b = "Hello world";
+let c = true;
+
+
+let iims;
+
+console.log(iims);
+
+let person = {
+    name: "Gaurav Maharjan",
+    age: 21,
+    address: "Kathmandu",
+    phone: "9800000000",
+    birthDate: new Date(),
+    location: {
+        lat: 27.21312312,
+        lng: 87.23443244
+    }
+}
+
+
+
+console.log("This person name is", person.name);
+console.log("This person name birthdate is", person.birthDate);
+console.log("This person location is", person.location.lng);
+
+
+let exampleArray = Array(1, 2, 3, 4, 5, 6, 7);
+
+console.log(exampleArray);
