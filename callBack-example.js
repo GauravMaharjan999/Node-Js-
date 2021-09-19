ae9da4ddf43939463512a66b39649f463f3ef064
Gaurav Maharjan <rijanmaharjan999@gmail.com>
2021-09-19 08:33:48 +0545

    call back example and assignment operater example

---
+++ callBack-example.js
@@ -0,0 +1,16 @@
+// const add = (a, b) => a + b;
+// const subtract = (a, b) => a - b;
+// const multiply = (a, b) => a * b;
+
+// const callBackExample = (a, b, mathCallBack) => {
+//     console.log(mathCallBack(a, b))
+// }
+
+// callBackExample(2, 2, add);
+
+
+if (1 !== "2") {
+    console.log("It is true")
+} else {
+    console.log("It is false")
+}
